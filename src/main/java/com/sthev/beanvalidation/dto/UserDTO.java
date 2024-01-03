855d070f7ed0a59628a07f73032ed8d8f33faa09
Stheven <sthevenalvess@gmail.com>
2024-01-03 10:57:04 -0300

    Camada UserDTO

---
+++ src/main/java/com/sthev/beanvalidation/dto/UserDTO.java
@@ -0,0 +1,88 @@
+package com.sthev.beanvalidation.dto;
+
+import java.time.Instant;
+
+import com.sthev.beanvalidation.entities.User;
+
+public class UserDTO {
+	
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private Instant birthDate;
+	private Double balance;
+	
+	public UserDTO() {
+		
+	}
+
+	public UserDTO(Long id, String name, String email, String phone, Instant birthDate, Double balance) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.birthDate = birthDate;
+		this.balance = balance;
+	}
+	
+	public UserDTO(User user) {
+		id = user.getId();
+		name = user.getName();
+		email = user.getEmail();
+		phone = user.getPhone();
+		birthDate = user.getBirthDate();
+		balance = user.getBalance();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+	
+	
+}
