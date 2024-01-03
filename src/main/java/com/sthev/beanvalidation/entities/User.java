1bce3d2d448a15d9d816ebd607c4b508b09d34fb
Stheven <sthevenalvess@gmail.com>
2024-01-03 10:48:41 -0300

    Entidade User e mapeamento

---
+++ src/main/java/com/sthev/beanvalidation/entities/User.java
@@ -0,0 +1,87 @@
+package com.sthev.beanvalidation.entities;
+
+import java.time.Instant;
+
+public class User {
+
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private Instant birthDate;
+	private Double balance;
+	private String password;
+	
+	public User() {
+		
+	}
+
+	public User(Long id, String name, String email, String phone, Instant birthDate, Double balance, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.birthDate = birthDate;
+		this.balance = balance;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
